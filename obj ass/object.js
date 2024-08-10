822d7a5896c6c92d8477b3162f41470ab6c15525
namtor4tech <158115456+namtor4tech@users.noreply.github.com>
2024-08-10 22:00:34 +0100

    Add files via upload

---
+++ obj ass/object.js
@@ -0,0 +1,190 @@
+ const company1 = {
+    companyname:"Apple Inc",
+    StartDate: " April 1, 1976",
+    enddate: "till date"
+ }
+
+ const company2 = {
+    companyname:"Microsoft Corporation",
+    StartDate: "  April 4, 1975",
+    enddate: "till date"
+ }
+
+ const company3 = {
+    companyname:"Google LLC",
+    StartDate: " September 4, 1998",
+    enddate: "till date"
+ }
+ const company4 = {
+    companyname:"Intel Corporation",
+    StartDate: "July 18, 1968 ",
+    enddate: "till date"
+ }
+ const company5 = {
+    companyname:"Cisco Systems, Inc.",
+    StartDate: " December 10, 1984",
+    enddate: "till date"
+ }
+
+ const company6= {
+    companyname:"Oracle Corporation",
+    StartDate: " Date: June 16, 1977",
+    enddate: "till date"
+ }
+
+ const company7 = {
+    companyname:"NVIDIA Corporation",
+    StartDate: "April 5, 1993",
+    enddate: "till date"
+ }
+
+ const company8 = {
+    companyname:"Uber Technologies, Inc",
+    StartDate: "March 2009",
+    enddate: "till date"
+ }
+
+ const company9 = {
+    companyname:"Twitter, Inc",
+    StartDate: " March 21, 2006",
+    enddate: "till date"
+ }
+
+ const company10 = {
+    companyname:"Airbnb, Inc.",
+    StartDate: "August 2008", 
+    enddate: "till date"
+ }
+
+ const company11 = {
+    companyname:"Tesla, Inc",
+    StartDate: "July 1, 2003",
+    enddate: "till date"
+ }
+
+ const company12 = {
+    companyname:"Alibaba Group Holding Limited",
+    StartDate: "April 4, 1999",
+    enddate: "till date"
+ }
+
+ const company13 = {
+    companyname:"Dell Technologies Inc",
+    StartDate: "February 1, 1984",
+    enddate: "till date"
+ }
+
+ const company14 = {
+    companyname:"Spotify AB",
+    StartDate: "April 23, 2006",
+    enddate: "till date"
+ }
+
+
+ const company15 = {
+    companyname:"Netflix, Inc.",
+    StartDate: "August 29, 1997",
+    enddate: "till date"
+ }
+
+
+ const company16 = {
+    companyname:"Sony Corporation",
+    StartDate: "Start: May 7, 1946",
+    enddate: "till date"
+ }
+
+
+ const company17 = {
+    companyname:"AT&T Inc",
+    StartDate: "March 3, 1983",
+    enddate: "till date"
+ }
+
+
+ const company18 = {
+    companyname:"eBay Inc.",
+    StartDate: "September 3, 1995",
+    enddate: "till date"
+ }
+
+
+ const company19 = {
+    companyname:"Sun Microsystems",
+    StartDate: "  February 24, 1982",
+    enddate: "January 27, 2010 "
+ }
+
+
+ const company20 = {
+    companyname:"Snap Inc.",
+    StartDate: "September 16, 2011",
+    enddate: "till date"
+ }
+
+
+ const company21 = {
+    companyname:"Facebook, Inc.",
+    StartDate: " October 28, 2021",
+    enddate: "till date"
+ }
+
+
+ const company22 = {
+    companyname:"Amazon.com, Inc.",
+    StartDate: " July 5, 1994",
+    enddate: "till date"
+ }
+
+ const company23 = {
+    companyname:"Motorola, Inc",
+    StartDate: " September 25, 1928",
+    enddate:"January 29, 2014 ",
+ }
+
+
+ const company24 = {
+    companyname:"Palm, Inc.",
+    StartDate: "March 28, 1992",
+    enddate: "April 28, 2011",
+ }
+
+ const company25 = {
+    companyname:"GitHub, Inc.",
+    StartDate: "April 10, 2008",
+    enddate: "till date"
+ }
+
+ const company26 = {
+    companyname:"Acer Inc.",
+    StartDate: "August 1, 1976",
+    enddate: "till date"
+ }
+
+ const company27 = {
+    companyname:"Philips Electronics",
+    StartDate: "May 15, 1891",
+    enddate: "till date"
+ }
+
+ const company28 = {
+    companyname:"Toshiba Corporation",
+    StartDate: " July 20, 1939",
+    enddate: "till date"
+ }
+
+ const company29 = {
+    companyname:"Zoom Video Communications, Inc",
+    StartDate: "April 21, 2011",
+    enddate: "till date"
+ }
+
+
+ const company30 = {
+    companyname:"Tencent Holdings Limited",
+    StartDate: "November 11, 1998",
+    enddate: "till date"
+
+ }
+
+
